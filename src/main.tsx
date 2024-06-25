cab6c7c52c787b945fd23b7217bf98b6b704a9cd
Sakagava Igor <sie1821@gmail.com>
2024-06-25 22:28:26 +0600

    К исходному (с JS файлом)

--- src/main.tsx
+++ src/main.tsx
@@ -15,8 +15,6 @@ import { Registration } from './pages/SignUp.js'
 import { PersistGate } from 'redux-persist/integration/react'
 import { LoginPage } from './pages/LoginPage.js'
 
-//Какой-то текст, который должен остаться
-
 const router = createBrowserRouter([
 	{
 		path: '/',
